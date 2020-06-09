4cdbb5caea5496ae5df34bd219b3000c6c1c5158
sitakura <scot.itakura@gmail.com>
2020-06-09 02:59:01 -0700

    added if and while loops for every array. added numTrue. added shuffle to end before alert

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,10 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-const specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
-console.log(specialCharacters);
-console.log(numericCharacters);
-console.log(lowerCasedCharacters);
-console.log(upperCasedCharacters);
 
 // Write password to the #password input
 function writePassword() {
@@ -18,20 +13,59 @@ function writePassword() {
   var hasSpecialchar = window.confirm("Would you like the password to have special characters?");
   var hasNumber = window.confirm("Would you like the password to have numbers?");
 
-  var passwordLength = parseInt(window.prompt("How many characters would you like your password to be between 8 and 128?"));
+  var passwordLength = parseInt(window.prompt("How many characters would you like your password to be? Please choose between 8 and 128."));
 
   if (passwordLength < 8) {
     window.alert("Password length must be greater than 8 characters.");
   }
 
-  if (passwordLength > 128){
+  if (passwordLength > 128) {
     window.alert("Password length must be less than 128 characters.");
   }
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var numTrue = ([hasLowercase,hasUppercase,hasSpecialchar,hasNumber].filter(v => v).length);
+  var passwordSet = new Set();
 
-  passwordText.value = password;
+  if(hasLowercase) {
+    while(passwordSet < passwordLength/numTrue) {
+      var randomIndex = Math.floor(Math.random() * (lowerCasedCharacters.length));
+      var character = lowerCasedCharacters[randomIndex];
+      passwordSet.add(character);
+    }
+  }
+
+  if(hasUppercase) {
+    while(passwordSet < passwordLength/numTrue) {
+      var randomIndex = Math.floor(Math.random() * (upperCasedCharacters.length));
+      var character = upperCasedCharacters[randomIndex];
+      passwordSet.add(character);
+    }
+  }
+
+  if(hasSpecialchar) {
+    while(passwordSet < passwordLength/numTrue) {
+      var randomIndex = Math.floor(Math.random() * (specialCharacters.length));
+      var character = specialCharacters[randomIndex];
+      passwordSet.add(character);
+    }
+  }
+
+  if(hasNumber) {
+    while(passwordSet < passwordLength/numTrue) {
+      var randomIndex = Math.floor(Math.random() * (numericCharacters.length));
+      var character = numericCharacters[randomIndex];
+      passwordSet.add(character);
+    }
+  }
+
+  shuffle(passwordSet);
+  var passwordString = passwordSet.toString();
+  window.alert(passwordString);
+
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
 
 }
 
